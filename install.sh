817fcb5a68dd4fb66ef408045ac4b3eb1c5b6edb
Ryan Gingerich <13091837+ryang3d@users.noreply.github.com>
2025-04-10 00:22:02 -0700

    Remove floodlight_ctl.sh install
    Remove floodlight_ctl.sh install since plan is to only use one or the other

--- install.sh
+++ install.sh
@@ -22,9 +22,6 @@ scp -i ${WYZECAM_KEY} ./bin/* root@${WYZECAMV3_HOST}:/media/mmc/mosquitto/bin
 scp -i ${WYZECAM_KEY} ./lib/* root@${WYZECAMV3_HOST}:/media/mmc/mosquitto/lib
 scp -i ${WYZECAM_KEY} mosquitto.conf root@${WYZECAMV3_HOST}:/media/mmc/mosquitto
 
-echo "Uploading floodlight control script to camera at ${WYZECAMV3_HOST}..."
-scp -i ${WYZECAM_KEY} floodlight_ctl.sh root@${WYZECAMV3_HOST}:/media/mmc/wz_mini/bin/wz_mini/bin
-
 echo "Installing MQTT client on camera..."
 ssh -i ${WYZECAM_KEY} root@${WYZECAMV3_HOST} '/media/mmc/mosquitto/installer/setup.sh'
 echo "Camera rebooting..."
